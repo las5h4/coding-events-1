b503ce007b1efc7f5eaba2e166fb9eb1ecafdfb7
las5h4 <loganasmalley@gmail.com>
2020-03-04 17:00:14 -0600

    Created AbstractEntity for id field, getId method, and equals/hashcode methods.

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,20 +1,15 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.Objects;
 
 /**
  * Created by Chris Bay
  */
 @Entity
 public class Event extends AbstractEntity {
-
-
 
     @NotBlank(message = "Name is required")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
@@ -70,14 +65,8 @@ public class Event extends AbstractEntity {
         this.type = type;
     }
 
-
-
     @Override
     public String toString() {
         return name;
     }
-
-
-
-
 }
